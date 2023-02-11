c63b627e1c059ad00b152f823a57d30a71681e8c
eddy <65038622+eddy39@users.noreply.github.com>
2023-02-12 00:03:15 +0100

    added passable objects

---
+++ Entities/Objects/PassableObjects/BasePassableObject.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+onready var colPol = get_node("CollisionPolygon2D")
+var player
+export var opacity = 0.5;
+func _ready():
+	pass # Replace with function body.
+
+
+func set_player(player_):
+	player = player_
+
+func _physics_process(delta):
+	# check if player assigned
+	if player == null: return
+	# check if player in polygon
+	if (Geometry.is_point_in_polygon(player.position-position,colPol.polygon)):
+		modulate = Color(1,1,1,opacity)
+	else:
+		modulate = Color(1,1,1,1)
+	
