97df76364eb8a0f2797861a9eec10d467f1bddab
Hayden Leete <haydenleete@gmail.com>
2023-02-11 14:33:26 +1300

    Add player sprite and change base level colliders

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export var move_speed := 500
+onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 
 var vel := Vector2()
 
@@ -9,3 +10,9 @@ func _physics_process(delta : float):
 	var target_vel = move.normalized() * move_speed
 	vel = vel.linear_interpolate(target_vel, delta * 10)
 	move_and_slide(vel)
+	var f = round(abs(Vector2.DOWN.angle_to(vel))/deg2rad(45))
+	if vel.x < 1:
+		animated_sprite.scale.x = 1
+	if vel.x > 1:
+		animated_sprite.scale.x = -1
+	animated_sprite.frame = f
