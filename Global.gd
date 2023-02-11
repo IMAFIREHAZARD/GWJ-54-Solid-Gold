509d47d707b92171e9606730bf5eaa58aac3b0ea
plexsoup <plexsoup@gmail.com>
2023-02-11 09:35:21 -0800

    added Eddy14's TestLevel to testing menu

--- Global.gd
+++ Global.gd
@@ -8,16 +8,6 @@ var bpm : float = 90.0
 func _ready():
 	pass # Replace with function body.
 
-################## CREATED PROBLEMS IN AUDIOSYSTMES ###########################
-################## probably not intended solution ############################
-func is_paused():
-	return get_tree().paused
-
-var bpm = 1;
-#########################################################################
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func is_paused():
 	
