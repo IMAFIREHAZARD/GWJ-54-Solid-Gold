b1f1e3a195a805e1e6e3aa1500809a3cffe6fe17
plexsoup <plexsoup@gmail.com>
2023-02-10 22:03:59 -0800

    Changed Inventory to Help

--- DesignNotes.gd
+++ DesignNotes.gd
@@ -9,9 +9,15 @@ Play goal: get through all the levels
 Roles:
 	Plexsoup = framework for menus, etc.
 	Disco = art for menus, etc.
-	Hayden = Player controller
+	Hayden = Player controller, shooting, npcs,
 	BitGhost = quest system
-	Eddy = tilemap and levels
+	Eddy = tilemap and level framework
+	Andy = level design
+	twilightrc = interactive Terrain (windows that smash or ricochet bullets)
+	
+Accessibility Guidelines:
+	https://drive.google.com/file/d/1YBCVIlETy_qSzvsYDQg_aaW0ZdKCG-kE/view?usp=sharing
+	
 	
 """
 
--- GUI/HelpFooter.gd
+++ GUI/HelpFooter.gd
@@ -8,7 +8,7 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Vbox/InventoryButton.text = "Inventory [ " + InputMap.get_action_list("toggle_inventory")[0].as_text() + " ]"
+	$Vbox/TogglePanelButton.text = "Help [ " + InputMap.get_action_list("help")[0].as_text() + " ]"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +16,8 @@ func _ready():
 #	pass
 
 func _unhandled_input(_event):
-	if Input.is_action_just_pressed("toggle_inventory"):
-		$Vbox/InventoryButton.pressed = !$Vbox/InventoryButton.pressed
+	if Input.is_action_just_pressed("help"):
+		$Vbox/TogglePanelButton.pressed = !$Vbox/TogglePanelButton.pressed
 		
 
 	
@@ -25,9 +25,9 @@ func _on_InventoryButton_toggled(button_pressed):
 	var pixel_offset : float = -1 * get_rect().size.y
 	var duration : float = 0.15 # seconds
 	if button_pressed:
-		pixel_offset = -1 * $Vbox/InventoryButton.get_rect().size.y
+		pixel_offset = -1 * $Vbox/TogglePanelButton.get_rect().size.y
 	var tween = get_tree().create_tween().set_trans(Tween.TRANS_BOUNCE)
 
 	tween.tween_property(self, "margin_top", pixel_offset, duration)
-	$InventoryOpenNoise.start()
+	$PanelOpenNoise.start()
 
