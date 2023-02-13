6db0041a86441b8dd4d69580573d6bf624dfefc9
Hayden Leete <haydenleete@gmail.com>
2023-02-14 00:51:59 +1300

    Bullet improvements

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -103,9 +103,9 @@ func get_affordance(affordanceName : String):
 func shoot():
 	reload_timer.start()
 	var bullet = bullet_scene.instance() as Node2D
-	bullet.rotation = get_local_mouse_position().angle()
 	get_parent().add_child(bullet)
-	bullet.global_position = global_position
+	bullet.global_position = $BulletSpawnPoint.global_position
+	bullet.rotation = bullet.get_local_mouse_position().angle() + rand_range(-0.1, 0.1)
 
 func start_gun_curse():
 	animated_sprite.frames = gun_hands_frames
