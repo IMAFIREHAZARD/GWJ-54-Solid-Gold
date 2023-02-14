485c83a011fd44636668796849f0cb29d1b7c3aa
plexsoup <plexsoup@gmail.com>
2023-02-14 00:44:14 -0800

    added secret exit from shooter

--- Levels/Run/RunnerLevel.gd
+++ Levels/Run/RunnerLevel.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 export var next_scene : PackedScene
+export var duration : float = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Timer/ExitTimer.set_wait_time(duration)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
