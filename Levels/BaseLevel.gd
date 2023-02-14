b87d65b7195f6fe11fa88724298b6e6f728eba7d
plexsoup <plexsoup@gmail.com>
2023-02-14 00:09:41 -0800

    quieting warnings

--- Levels/BaseLevel.gd
+++ Levels/BaseLevel.gd
@@ -12,14 +12,14 @@ func _ready():
 	assign_player2passObjs()
 
 func assign_player2passObjs():
-	if has_node("tileMapLevel1"):
+	if tileMapLevel1 != null:
 		for child in tileMapLevel1.get_children():
 			if child.has_method("set_player"):
 				child.set_player(player)
 			pass
 
 func get_tile_underneath(pos_global:Vector2):
-	if has_node("tileMapGround") == false:
+	if tileMapGround == null:
 		return
 		
 	var local_position = tileMapGround.to_local(pos_global)
