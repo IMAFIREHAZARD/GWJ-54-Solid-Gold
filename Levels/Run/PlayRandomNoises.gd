8818b6cbbda48542d167a828482e69adc1d43973
plexsoup <plexsoup@gmail.com>
2023-02-13 20:13:49 -0800

    footsteps in runner

---
+++ Levels/Run/PlayRandomNoises.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func play_random_noise():
+	var noises = get_children()
+	var randomNoise = noises[randi()%noises.size()]
+	randomNoise.start()
