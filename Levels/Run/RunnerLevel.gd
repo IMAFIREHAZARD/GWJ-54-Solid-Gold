f78b7ac2ce63e59ca5cf10c1426dcc6bccae382c
plexsoup <plexsoup@gmail.com>
2023-02-14 03:09:18 -0800

    runner -> catwalks -> sokoban

--- Levels/Run/RunnerLevel.gd
+++ Levels/Run/RunnerLevel.gd
@@ -2,10 +2,41 @@ extends Node2D
 
 export var next_scene : PackedScene
 export var duration : float = 5.0
+export (String, "TimelineDropdown") var timeline
+
+var bargain_offered : bool = false
+
+
+#signal accept_curse()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer/ExitTimer.set_wait_time(duration)
+	
+
+func offer_devils_bargain():
+	$ObstacleSpawnTimer.set_paused(true)
+	var new_dialog = Dialogic.start(timeline)
+	add_child(new_dialog)
+	new_dialog.connect("dialogic_signal", self, "_on_dialogic_signal")
+
+func _on_dialogic_signal(param):
+	if param == "restart_level":
+		Global.reset_curses()
+		StageManager.restart_current_level()
+		
+	elif param == "speed_up":
+		speed_up()
+	elif param == "do_not_speed_up":
+		Global.speed_curse_taken = false
+	$ObstacleSpawnTimer.set_paused(false)
+	$Timer/ExitTimer.start()
+	
+	
+func speed_up():
+	Global.speed_curse_taken = true
+	$Floor/Sprite.speed *= 2.0
+	# PlayerSideView will handle it's own speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -26,4 +57,16 @@ func _on_Timer_timeout():
 
 
 func _on_ExitTimer_timeout():
-	StageManager.change_scene_to(next_scene)
+	
+	if bargain_offered == false:
+		offer_devils_bargain()
+		bargain_offered = true
+	else:
+		StageManager.change_scene_to(next_scene)
+
+func _on_runner_died():
+	var new_dialog = Dialogic.start("PlayerDied")
+	add_child(new_dialog)
+	new_dialog.connect("dialogic_signal", self, "_on_dialogic_signal")
+	$Timer/ExitTimer.stop()
+	$ObstacleSpawnTimer.stop()
