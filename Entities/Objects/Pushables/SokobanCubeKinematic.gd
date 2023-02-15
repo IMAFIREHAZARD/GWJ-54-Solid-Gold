876aa761347e656837b1141302ff749d7849af30
plexsoup <plexsoup@gmail.com>
2023-02-14 23:25:14 -0800

    tweaking new hope

--- Entities/Objects/Pushables/SokobanCubeKinematic.gd
+++ Entities/Objects/Pushables/SokobanCubeKinematic.gd
@@ -34,10 +34,11 @@ func _ready():
 	
 func set_move_dist():
 	# pythagoras
-	var x = $Sprite.get_rect().size.x / 2.0 #* global_scale.x 
-	var y = $Sprite.get_rect().size.y / 2.0 #* global_scale.y
+	var x = $Sprite.get_rect().size.x / 2.0 * global_scale.x 
+	var y = $Sprite.get_rect().size.y / 4.0 * global_scale.y
 	var hypoteneuse = sqrt(x*x + y*y)
-	move_dist = hypoteneuse
+	print("pythagoras says: ", hypoteneuse, " but we're setting it to 71 because the ruler tool measured that.")
+	move_dist = 71.0 / 2.0 # why /2???
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
