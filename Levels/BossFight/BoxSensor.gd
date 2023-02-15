69cdb72267867a5e688aecd0b81b45927dc95f5f
Hayden Leete <haydenleete@gmail.com>
2023-02-15 18:01:51 +1300

    Improved Boxing picking to the max

--- Levels/BossFight/BoxSensor.gd
+++ Levels/BossFight/BoxSensor.gd
@@ -5,12 +5,12 @@ signal block_exited(block)
 
 func _on_BoxSensor_body_entered(body: Node) -> void:
 	print(body)
-	if body is SokobanBlock:
+	if body is PushBlock:
 		emit_signal("block_entered", body)
 		$Line2D.default_color = Color.greenyellow
 
 
 func _on_BoxSensor_body_exited(body: Node) -> void:
-	if body is SokobanBlock:
+	if body is PushBlock:
 		emit_signal("block_exited", body)
 		$Line2D.default_color = Color.cornflower
---
+++ Levels/Sokoban/SokobanSelector.gd
@@ -0,0 +1,26 @@
+# an autoload that manages mouse picking for sokoban blocks
+extends Node
+
+var hovered_blocks := []
+var front_hovered_block : PushBlock = null
+
+func _ready() -> void:
+	set_physics_process_internal(true)
+
+func _notification(what: int) -> void:
+	# Happens right after viewport picking
+	if what == NOTIFICATION_INTERNAL_PHYSICS_PROCESS:
+		
+		if hovered_blocks.empty():
+			if front_hovered_block:
+				front_hovered_block.set_highlighted(false)
+			front_hovered_block = null
+			return
+		# find hovered block with highest y position
+		var front_block = hovered_blocks[0]
+		for block in hovered_blocks:
+			if block.global_position.y > front_block.global_position.y:
+				front_block = block
+		if front_hovered_block and front_block != front_hovered_block:
+			front_hovered_block.set_highlighted(false)
+		front_hovered_block = front_block
