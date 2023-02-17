893f306bc0a4055b1283782c0d9015548ce38739
Hayden Leete <haydenleete@gmail.com>
2023-02-17 23:39:49 +1300

    Add boss fight intro cutscnene

--- Levels/BossFight/BossFight.gd
+++ Levels/BossFight/BossFight.gd
@@ -2,3 +2,29 @@ extends "res://Levels/BaseLevel.gd"
 
 var current_bugs = 0
 var max_bugs = 3
+onready var shield_tower: StaticBody2D = $"%ShieldTower"
+onready var shield_tower_2: StaticBody2D = $"%ShieldTower2"
+onready var boss: Sprite = $YSort/BossRoot/pos/Boss
+
+func _ready() -> void:
+	player.set_physics_process(false)
+	player.set_process_unhandled_input(false)
+	yield(do_cutscene(), "completed")
+	boss.get_node("Timer").start()
+	player.set_physics_process(true)
+	player.set_process_unhandled_input(true)
+
+func do_cutscene() -> void:
+	yield(get_tree().create_timer(0.5), "timeout")
+	yield(pan_camera(boss.global_position), "completed")
+	var dialog = spawn_dialog("BossIntro")
+	yield(dialog, "dialogic_signal")
+	pan_camera(shield_tower_2.global_position)
+	yield(dialog, "dialogic_signal")
+	yield(pan_camera(player.global_position), "completed")
+	
+
+func pan_camera(target : Vector2, time := 1.0):
+	var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(cam, "global_position", target, time)
+	yield(tween, "finished")
