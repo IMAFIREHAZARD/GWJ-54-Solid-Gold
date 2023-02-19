690c0f56f1b0392f97fada09b911b2eb3d08db8f
plexsoup <plexsoup@gmail.com>
2023-02-18 23:41:17 -0800

    remove player stun
    replaced with knockback - WIP

--- Levels/BossFight/BossStunAttack.gd
+++ Levels/BossFight/BossStunAttack.gd
@@ -4,7 +4,10 @@ export var speed_mulitplier = 0.3
 func _on_BurstDelay_timeout() -> void:
 	$AnimationPlayer.play("Burst")
 	if overlaps_body(StageManager.player):
-		StageManager.player.stun(2)
+		#StageManager.player.stun(2)
+		var impactVector = global_position - StageManager.player.global_position
+		var impactMagnitude = 200.0
+		StageManager.player.knockback(impactVector * impactMagnitude)
 	yield($AnimationPlayer, "animation_finished")
 	queue_free()
 
